2914e53a8e387c3faab97b2b7c239be06e31330f
Dejvi <dejviepadejvi@gmail.com>
2023-07-31 13:06:32 +0200

    shtuam context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Popular from './components/Popular'
+
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Popular />
-    </div>
+    </Router>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
+import { GlobalContextProvider } from './context/global';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalContextProvider>
+     <App />
+    </GlobalContextProvider>
   </React.StrictMode>
 );
 
