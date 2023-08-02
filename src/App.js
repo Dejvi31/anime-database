dd15f0af889e57a8be767edb16c48d22d0a44d17
ReiEpa <reieparei@gmail.com>
2023-08-02 11:49:09 +0200

    app chang

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-// import Popular from './components/Popular'
-// import AnimeItem from './components/AnimeItem'
-
-import Homepage from './components/Homepage'
+import Popular from './components/Popular'
+import AnimeItem from './components/AnimeItem'
 
 
 const App = () => {
   return (
     <Router>
-      <Homepage />
+      <Routes>
+        <Route path='/' element={<Popular />} />
+        <Route path='/anime/:id' element={<AnimeItem />}/>
+      </Routes>
+      
     </Router>
   )
 }
